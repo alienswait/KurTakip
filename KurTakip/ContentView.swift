aad3b5bca9379c43982e223e62953f80e9881544
Mert can Ünek <mrtcnunk@gmail.com>
2026-09-01 21:27:17 +0300

    Initial Commit

---
+++ KurTakip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KurTakip
+//
+//  Created by Mertcan Ünek on 1.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KurTakip/KurTakipApp.swift
@@ -0,0 +1,17 @@
+//
+//  KurTakipApp.swift
+//  KurTakip
+//
+//  Created by Mertcan Ünek on 1.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct KurTakipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
